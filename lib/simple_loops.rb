e0cd353f33b674386c4af661e86ef180666bb970
Hugo Rodrigez <hugor37@gmail.com>
2020-08-02 20:32:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,8 +2,8 @@ def loop_message_five_times(string)
   counter = 0
   array = []
 
-  while array[string] do
-    puts array[string]
+  while array[counter] do
+    puts array[counter]
     counter += 1
   end
 end
