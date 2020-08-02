8579b12a95ae70dc9d4284ac31fafcbc41240d22
Hugo Rodrigez <hugor37@gmail.com>
2020-08-02 20:36:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times(string)
     count += 1
   end
 end
+
+def loop_message_five_times(string)
+  count = 0
+  while count < 5 do
+    puts string
+    count += 1
+  end
+end
